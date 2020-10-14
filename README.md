89b3c5565b2e0d04dbed84952f183edbb232b863
YashChoudhary443 <72564001+YashChoudhary443@users.noreply.github.com>
2020-10-14 16:55:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Bouncy-ball-physics-engine-
+# staticSquare
+A static square made using Matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground;
+var ball;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,400,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1
+    }
+
+    ball = Bodies.circle(200,200,15,ball_options);
+    World.add(world,ball);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+
+    ellipse(ball.position.x,ball.position.y,30,30);
+}
